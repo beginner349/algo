ed045d6df324d4c10ca278f5bd2b039c126ad62b
jiajun <cjjjunjiajun@gmail.com>
2020-02-07 22:10:28 +0800

    [Easy] Merge Sorted Array

---
+++ src/algo/leetcode/Easy/Merge_Sorted_Array/Solution.java
@@ -0,0 +1,25 @@
+package algo.leetcode.Easy.Merge_Sorted_Array;
+
+class Solution {
+
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+        m = m - 1;
+        n = n - 1;
+        int index = nums1.length - 1;
+        while (m >= 0 && n >= 0) {
+        	if (nums1[m] > nums2[n]) {
+        		nums1[index] = nums1[m];
+        		m--;
+        	} else {
+        		nums1[index] = nums2[n];
+        		n--;
+        	}
+    		index--;
+        }
+        while (n >= 0) {
+        	nums1[index] = nums2[n];
+        	index--;
+        	n--;
+        }
+    }
+}
