ce7a8dcaca02fdfbc0ac49aaab937cc41fda1a81
jiajun <cjjjunjiajun@gmail.com>
2020-06-28 09:33:52 +0800

    [Easy] Best Time to Buy and Sell Stock II

---
+++ src/algo/leetcode/Easy/Best_Time_to_Buy_and_Sell_Stock_II/Solution.java
@@ -0,0 +1,15 @@
+package algo.leetcode.Easy.Best_Time_to_Buy_and_Sell_Stock_II;
+
+class Solution {
+
+	public int maxProfit(int[] prices) {
+		int profit = 0;
+		for (int i = 1; i < prices.length; i++) {
+			if (prices[i] > prices[i-1]) {
+				profit = profit + (prices[i] - prices[i-1]);
+			}
+		}
+		return profit;
+	}
+
+}
