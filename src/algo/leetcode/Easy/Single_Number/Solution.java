d6998d3d7cec2812641c4ae94cf0c81a5f047aba
jiajun <cjjjunjiajun@gmail.com>
2020-07-05 15:59:58 +0800

    [Easy] Single Number

---
+++ src/algo/leetcode/Easy/Single_Number/Solution.java
@@ -0,0 +1,25 @@
+package algo.leetcode.Easy.Single_Number;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+
+	public int singleNumber(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(nums[i])) {
+				map.put(nums[i], map.get(nums[i]) + 1);
+			} else {
+				map.put(nums[i], 1);
+			}
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (map.get(nums[i]) == 1) {
+				return nums[i];
+			}
+		}
+		return -1;
+	}
+
+}
