afd473d609c7f04e55d97855ccefc2fd1ca1dfb5
jiajun <cjjjunjiajun@gmail.com>
2020-07-11 12:05:44 +0800

    [Medium] Binary Tree Preorder Traversal

---
+++ src/algo/leetcode/Medium/Binary_Tree_Preorder_Traversal/Solution.java
@@ -0,0 +1,26 @@
+package algo.leetcode.Medium.Binary_Tree_Preorder_Traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+
+	public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> result = new ArrayList<Integer>();
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		while (true) {
+			if (root != null) {
+				result.add(root.val);
+				stack.add(root.right);
+				root = root.left;
+			} else if (!stack.isEmpty()) {
+				root = stack.pop();
+			} else {
+				break;
+			}
+		}
+		return result;
+		
+	}
+}
---
+++ src/algo/leetcode/Medium/Binary_Tree_Preorder_Traversal/TreeNode.java
@@ -0,0 +1,21 @@
+package algo.leetcode.Medium.Binary_Tree_Preorder_Traversal;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode() {
+	}
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+
+}
