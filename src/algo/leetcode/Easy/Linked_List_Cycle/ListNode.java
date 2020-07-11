41f32317aad466c389026ca55d8526a6a8093e54
jiajun <cjjjunjiajun@gmail.com>
2020-07-11 11:10:26 +0800

    [Easy] Linked List Cycle

---
+++ src/algo/leetcode/Easy/Linked_List_Cycle/ListNode.java
@@ -0,0 +1,12 @@
+package algo.leetcode.Easy.Linked_List_Cycle;
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+	
+}
---
+++ src/algo/leetcode/Easy/Linked_List_Cycle/Solution.java
@@ -0,0 +1,18 @@
+package algo.leetcode.Easy.Linked_List_Cycle;
+
+class Solution {
+
+	public boolean hasCycle(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
